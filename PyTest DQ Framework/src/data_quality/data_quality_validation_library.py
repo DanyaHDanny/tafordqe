687e07d2df19fd3d19de709c18922e9bf20cf91e
Moskaltsou, Daniil <dmoskaltsou@estee.com>
2025-09-08 22:04:43 +0300

    updates

--- PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
+++ PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
@@ -12,18 +12,24 @@ class DataQualityLibrary:
 
     @staticmethod
     def check_duplicates(df, column_names=None):
+        if column_names:
+            df.duplicates(column_names)
+        else:
+            df.duplicates(all_columns)
 
     @staticmethod
     def check_count(df1, df2):
+        df1.count = df2.count
 
     @staticmethod
-    def check_data_completeness(df1, df2):
-
-    @staticmethod
-    def check_dataset_is_empty(df):
+    def check_data_full_data_set(df1, df2):
+        df1 = df2
 
     @staticmethod
     def check_dataset_is_not_empty(df):
+        df.is_not_empty
 
     @staticmethod
     def check_not_null_values(df, column_names=None):
+        col for df.column_names:
+            col.not_null
